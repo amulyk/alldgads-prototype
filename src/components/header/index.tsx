1c5deaa83ff31db880978c1a2ad128927bf6f429
Andrii Mulyk <amulyk@lectrum.io>
2019-10-28 22:09:51 +0200

    create initial base

---
+++ src/components/header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles/index.module.scss';
+
+export const Header: React.FC = () => {
+    return (
+        <section className={styles.header}>
+            <h1>Header!</h1>
+        </section>
+    );
+};
