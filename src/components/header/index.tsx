b395f1a45d4b6405664318616ba9f299cd9495bb
Andrii Mulyk <amulyk@lectrum.io>
2019-11-04 18:55:43 +0400

    feat(source): add cz

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,6 +3,6 @@ import styles from './styles/index.module.scss';
 
 export const Header: React.FC = () => (
   <section className={styles.header}>
-    <h1>Header !</h1>
+    <h1>Header!</h1>
   </section>
 );
