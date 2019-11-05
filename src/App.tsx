2a2398fd584058adfc747f88a3dfaa053bcedaee
Andrii Mulyk <amulyk@lectrum.io>
2019-11-06 01:26:42 +0400

    feat(source): fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Header } from './components/header';
 
-export const App: React.FC = () => <Header />;
+export const App: React.FC = () => <Header title="Welcome!" />;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 import styles from './styles/index.module.scss';
 
-export const Header: React.FC = () => (
-  <section className={styles.header}>
-    <h1>Header!</h1>
-  </section>
-);
+type Props = {
+  title?: string;
+  children?: never;
+};
+
+export const Header: FC<Props> = ({ title }: Props): ReactElement => {
+  const content = typeof title === 'string' ? title.toLocaleLowerCase() : title;
+  const [counter, setCounter] = useState(0);
+
+  const increase = (): void => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const decrease = (): void => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+
+  return (
+    <section className={styles.header}>
+      <h1>{content}</h1>
+      <button type="button" onClick={decrease}>
+        -
+      </button>
+      <h3>{counter}</h3>
+      <button type="button" onClick={increase}>
+        +
+      </button>
+    </section>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Hello!',
+};
