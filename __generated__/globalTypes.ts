d031c840539f51c3f7076356444469c8c990933d
Andrii Mulyk <amulyk@lectrum.io>
2019-12-02 23:47:34 +0200

    feat(source): add mutations

--- __generated__/globalTypes.ts
+++ __generated__/globalTypes.ts
@@ -6,6 +6,17 @@
 // START Enums and Input Objects
 //==============================================================
 
+/**
+ * When a user creates an account, they must send `name`, `username`, and
+ * `password` to the `createAccount` mutation. This input type wraps those fields
+ * into one fancy object.
+ */
+export interface CreateAccountInput {
+  name: string;
+  username: string;
+  password: string;
+}
+
 //==============================================================
 // END Enums and Input Objects
 //==============================================================
--- src/bus/pet/index.tsx
+++ src/bus/pet/index.tsx
@@ -1,8 +1,18 @@
 import React, { FC, ReactElement } from 'react';
 import { useLoaderData } from './hooks/useLoaderData';
+import { useCreateAccount } from './hooks/useCreateAccount';
 
 export const Pet: FC = (): ReactElement => {
   const { loading, error, data } = useLoaderData();
+  const {
+    saveAccount,
+    handleChange,
+    createdAccount,
+  } = useCreateAccount();
+
+  const save = (): void => {
+    saveAccount();
+  };
 
   if (loading) {
     return <p>Loading...</p>;
@@ -16,6 +26,13 @@ export const Pet: FC = (): ReactElement => {
     );
   }
 
+  const createdAccountJSX = createdAccount && (
+    <p>
+      Создан пользователь с именем:
+      { createdAccount.name }
+    </p>
+  );
+
   return (
     <section>
       <h1>Pets</h1>
@@ -23,6 +40,11 @@ export const Pet: FC = (): ReactElement => {
         AvailablePets:
         {data.availablePets}
       </p>
+      <input type="text" placeholder="name" name="name" onChange={handleChange} />
+      <input type="text" placeholder="username" name="username" onChange={handleChange} />
+      <input type="password" placeholder="password" name="password" onChange={handleChange} />
+      <button type="submit" onClick={save}>Save</button>
+      { createdAccountJSX }
     </section>
   );
 };
