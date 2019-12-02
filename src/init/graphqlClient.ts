61fe2166cdefaefed5c97e1acfd32fa2fcd68eb5
Andrii Mulyk <amulyk@lectrum.io>
2019-12-03 00:09:14 +0200

    feat(source): add cached

--- src/init/graphqlClient.ts
+++ src/init/graphqlClient.ts
@@ -1,5 +1,7 @@
 import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const client = new ApolloClient({
   uri: 'https://funded-pet-library.moonhighway.com/',
+  cache: new InMemoryCache(),
 });
