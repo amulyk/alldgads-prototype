a16bee702914333a6e1f89d49a538bacc76d28dd
Andrii Mulyk <amulyk@lectrum.io>
2019-12-03 08:13:13 +0200

    feat(source): add cache

---
+++ src/bus/pet/hooks/useLoaderData/gql/__generated__/PET_QUERY_AVAILABLE_PETS_COPY.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: PET_QUERY_AVAILABLE_PETS_COPY
+// ====================================================
+
+export interface PET_QUERY_AVAILABLE_PETS_COPY {
+  /**
+   * This query returns the number of `Pet`s that are available for checkout.
+   */
+  availablePets: number;
+}
--- src/bus/pet/hooks/useLoaderData/index.ts
+++ src/bus/pet/hooks/useLoaderData/index.ts
@@ -1,14 +1,16 @@
 // Core
 import { ApolloError } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
 import { loader } from 'graphql.macro';
 // import { gql } from 'apollo-boost';
 
 // Types
 import { PET_QUERY_AVAILABLE_PETS } from './gql/__generated__/PET_QUERY_AVAILABLE_PETS';
+import { PET_QUERY_AVAILABLE_PETS_COPY } from './gql/__generated__/PET_QUERY_AVAILABLE_PETS_COPY';
 
 // Queries
 const queryAvailablePets = loader('./gql/queryAvailablePets.graphql');
+const queryAvailablePetsCopy = loader('./gql/queryAvailablePetsCopy.graphql');
 // const queryAvailablePets = gql`
 //     query PET_QUERY_AVAILABLE_PETS {
 //         availablePet
@@ -23,6 +25,16 @@ type UseLoaderDataType = {
 
 export const useLoaderData = (): UseLoaderDataType => {
   const { loading, error, data } = useQuery(queryAvailablePets);
+  const [getPets, { loading: l, data: temp }] = useLazyQuery(queryAvailablePetsCopy);
+
+  if (!l) {
+    console.log(temp);
+  }
+
+  setTimeout(() => {
+    getPets();
+    console.log('timer', temp);
+  }, 2000);
 
   return { loading, error, data };
 };
