8816c7f4ddb47d762d85d505a7145d8af353ea83
Andrii Mulyk <amulyk@lectrum.io>
2019-12-05 06:30:17 +0200

    feat(source): add files

--- src/bus/pet/hooks/useLoaderData/index.ts
+++ src/bus/pet/hooks/useLoaderData/index.ts
@@ -1,13 +1,12 @@
 // Core
 import { ApolloError } from 'apollo-boost';
-import { useQuery, useLazyQuery } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 import { loader } from 'graphql.macro';
 // import { gql } from 'apollo-boost';
 
 // Types
 import { PET_QUERY_AVAILABLE_PETS } from './gql/__generated__/PET_QUERY_AVAILABLE_PETS';
 import { PET_QUERY_AVAILABLE_PETS_COPY } from './gql/__generated__/PET_QUERY_AVAILABLE_PETS_COPY';
-import {useEffect} from "react";
 
 // Queries
 const queryAvailablePets = loader('./gql/queryAvailablePets.graphql');
@@ -26,19 +25,18 @@ type UseLoaderDataType = {
 
 export const useLoaderData = (): UseLoaderDataType => {
   const { loading, error, data } = useQuery(queryAvailablePets);
-  // const { loading: l, data: temp } = useQuery(queryAvailablePetsCopy);
-  const [getPets, { loading: l, data: temp, called }] = useLazyQuery(queryAvailablePetsCopy);
+  const { loading: l, data: temp } = useQuery(queryAvailablePetsCopy);
+  // const [getPets, { loading: l, data: temp }] = useLazyQuery(queryAvailablePetsCopy);
 
-  console.log('hook');
 
   if (!l) {
-    console.log('timer', temp);
+    console.log(temp);
   }
 
-  setTimeout(() => {
-    getPets();
-    console.log('timer', temp);
-  }, 2000);
+  // setTimeout(() => {
+  //   getPets();
+  //   console.log('timer', temp);
+  // }, 2000);
 
   return { loading, error, data };
 };
